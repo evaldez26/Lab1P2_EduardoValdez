4f5c030b6400cfb7ba4cbf279f30a7b838d125b9
evaldez26 <gualitovo2006@gmail.com>
2024-04-19 14:22:26 -0600

    commit 2

--- Lab1P2_EduardoValdez/src/lab1p2_eduardovaldez/Lab1P2_EduardoValdez.java
+++ Lab1P2_EduardoValdez/src/lab1p2_eduardovaldez/Lab1P2_EduardoValdez.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab1p2_eduardovaldez;
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -11,6 +12,7 @@ import java.util.Random;
  * @author guali
  */
 public class Lab1P2_EduardoValdez {
+
     static Random rand = new Random();
     static Scanner leer = new Scanner(System.in);
 
@@ -20,33 +22,61 @@ public class Lab1P2_EduardoValdez {
     public static void main(String[] args) {
         System.out.print("Ingrese el tamaño de la matriz: ");
         int size = leer.nextInt();
-        if (size>4 && size%2 != 0){
-          int matrix [][] = generarmatriz(size);
-        imprimir(matrix);  
-        }else{
+        int matrix[][] = generarmatriz(size);
+        if (size > 4 && size % 2 != 0) {
+            imprimir(matrix);
+        } else {
             System.out.println("El tamaño debe ser mayor a 4, y debe ser un numero impar");
         }
-        
-        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                bubblesort(matrix[j]);
+                imprimir2(matrix[i]);
+            }
+        }
+
     }
-     public static int[][] generarmatriz(int tamaño){
-         int matriz[][] = new int [tamaño][tamaño];
-         for(int i = 0; i<tamaño; i++){
-             for (int j = 0; j<tamaño; j++){
+
+    public static int[][] generarmatriz(int tamaño) {
+        int matriz[][] = new int[tamaño][tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
                 int numero = rand.nextInt(10);
-                matriz[i][j]=numero;
-             }
-         }
-         return matriz;
-     }
-     public static void imprimir (int[][] matriz){
-         for(int i = 0; i<matriz.length;i++){
-             for (int j = 0; j<matriz.length;j++){
-                 System.out.print("["+matriz[i][j]+"]");
-             }
-             System.out.println();
-         }
-     }
-    
-    
+                matriz[i][j] = numero;
+            }
+        }
+        return matriz;
+    }
+
+    public static void imprimir(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print("[" + matriz[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void imprimir2(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("[" + arreglo[i] + "]");
+        }
+    }
+
+    public static void bubblesort(int[] arreglo) {
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo.length-1; j++) {
+                if (arreglo[j] > arreglo[j + 1]) {
+                    num1 = arreglo[j];
+                    num2 = arreglo[j + 1];
+                    arreglo[j] = num2;
+                    arreglo[j + 1] = num1;
+                }
+            }
+        }
+
+    }
+
 }
